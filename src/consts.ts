cc9e589419653f765956857829dde7a8a7b72552
caffeineaddict <caffeineaddict@rays-homelab.tail36b0aa.ts.net>
2025-10-12 14:49:34 +1100

    added personal deets

--- src/consts.ts
+++ src/consts.ts
@@ -22,7 +22,7 @@ export const KNOWN_TECH = [
 ];
 
 export const ABOUT_ME =
-  `wassup, I'm ray! I'm a CS student studying in Australia ;) 
+  `sup, I'm ray! I'm a CS student studying in Australia ;) 
   I'm pretty interested in threat intelligence (ask me about APT44!!), but just *very* recently I've been trying to develop cloud skills because... why not lol? 
   Hence this website. (it's my third attempt at making a personal website.) stay tuned!
 
@@ -36,6 +36,10 @@ export const NAV_LINKS: Array<{ title: string; href?: string }> = [
   {
     title: "Github",
     href: "//github.com/" + GITHUB_USERNAME,
+  },
+  {
+    title: "Email",
+    href: "rayneorshine03@gmail.com",
   },
   {
     title: "Resume",
